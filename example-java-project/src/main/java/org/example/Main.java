78c6723a4b8eba3ccfbf8b4a6f4bf8ecece75cc2
Andrei Poliakov <avpgenium@yandex.ru>
2022-07-14 02:26:13 +0300

    add maven checkstyle

--- example-java-project/src/main/java/org/example/Main.java
+++ example-java-project/src/main/java/org/example/Main.java
@@ -1,7 +1,10 @@
 package org.example;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+    User user = new User();
+    user.name = "Vasya";
+    user.age = 19;
+  }
 }
--- example-java-project/src/main/java/org/example/User.java
+++ example-java-project/src/main/java/org/example/User.java
@@ -1,6 +1,6 @@
 package org.example;
 
 public class User {
-    String name;
-    Integer age;
+  String name;
+  Integer age;
 }
